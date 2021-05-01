bc75ea19be878b28a74693f03d4da44280a587e4
Giovanni Londero <londero.giovanni@gmail.com>
2021-05-01 12:03:48 +0200

    feat(.env): add a .env example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/presentation/_core/app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dot_env;
 
-void main() {
+Future<void> main() async {
+  // * For the purpose of the project, only the default file '.env' wil be used.
+  await dot_env.load();
+
   runApp(const App());
 }
