f72d99e46ad21495e1c5bff34c8cc5e5b104c73f
Giovanni Londero <londero.giovanni@gmail.com>
2021-05-01 13:24:08 +0200

    feat(weather_repository): initial support for API get

--- lib/domain/i_weather_repository.dart
+++ lib/domain/i_weather_repository.dart
@@ -1,8 +1,8 @@
 import 'package:weather_app/domain/weather_api_response.dart';
 
 abstract class IWeatherRepository {
-  WeatherApiResponse getWeatherByCoordinates(Coordinates coordinates);
-  WeatherApiResponse getWeatherByCity({
+  Future<WeatherApiResponse> getWeatherByCoordinates(Coordinates coordinates);
+  Future<WeatherApiResponse> getWeatherByCity({
     required final String name,
     final String? stateCode,
     final String? countryCode,
--- lib/infrastructure/weather_repository.dart
+++ lib/infrastructure/weather_repository.dart
@@ -1,20 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/domain/i_weather_repository.dart';
 import 'package:weather_app/domain/weather_api_response.dart';
 
 class WeatherRepository implements IWeatherRepository {
+  static const _apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+  static final _apiKey = env["API_KEY"] ?? '';
+
   @override
-  WeatherApiResponse getWeatherByCity({
+  Future<WeatherApiResponse> getWeatherByCity({
     required String name,
     String? stateCode,
     String? countryCode,
-  }) {
-    // TODO: implement getWeatherByCity
-    throw UnimplementedError();
+  }) async {
+    final queryValues = [
+      name,
+      stateCode,
+      countryCode,
+    ].whereType<String>().join(',');
+
+    final response = await _getWeather(<String, dynamic>{
+      'q': queryValues,
+    });
+
+    // TODO: gestire errore o response null
+
+    return WeatherApiResponse.fromJson(response ?? <String, dynamic>{});
   }
 
   @override
-  WeatherApiResponse getWeatherByCoordinates(Coordinates coordinates) {
-    // TODO: implement getWeatherByCoordinates
-    throw UnimplementedError();
+  Future<WeatherApiResponse> getWeatherByCoordinates(
+      Coordinates coordinates) async {
+    final response = await _getWeather(<String, dynamic>{
+      'lat': coordinates.latitude,
+      'lon': coordinates.longitude,
+    });
+
+    // TODO: gestire errore o response null
+
+    return WeatherApiResponse.fromJson(response ?? <String, dynamic>{});
+  }
+
+  Future<Map<String, dynamic>?> _getWeather(
+      Map<String, dynamic> queryParameters) async {
+    final response = await Dio().get<Map<String, dynamic>>(
+      _apiUrl,
+      queryParameters: <String, dynamic>{
+        'appid': _apiKey,
+        ...queryParameters,
+      },
+    );
+
+    return response.data;
   }
 }
