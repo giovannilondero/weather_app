3b92c356a255681021305894dae5b7fc881d0c7a
Giovanni Londero <londero.giovanni@gmail.com>
2021-05-04 21:44:12 +0200

    feat: support search bar

---
+++ lib/presentation/home/components/today_details.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_app/domain/full_day_weather_details.dart';
+import 'package:weather_app/domain/weather_api_response.dart';
+import 'package:weather_app/presentation/_components/weather_icon.dart';
+
+class TodayDetails extends StatelessWidget {
+  final ResponseCity city;
+  final FullDayWeatherDetails day;
+
+  const TodayDetails({
+    Key? key,
+    required this.day,
+    required this.city,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+
+    return Column(
+      children: [
+        Text(city.name),
+        Text(DateFormat('EEE, d MMMM HH:mm').format(now)),
+        Row(
+          children: [
+            if (day.icon != null) WeatherIcon(code: day.icon!),
+            Text('${day.dayDetails.elementAt(0).main.temp.round()}°'),
+            Text(day.dayDetails.elementAt(0).weather.elementAt(0).description),
+          ],
+        ),
+        SizedBox(
+          height: 100,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: day.dayDetails.length,
+            itemBuilder: (context, index) {
+              final element = day.dayDetails.elementAt(index);
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(DateFormat('HH:mm').format(
+                      DateTime.fromMillisecondsSinceEpoch(element.dt * 1000))),
+                  Text('${element.main.temp.round()}°'),
+                  WeatherIcon(
+                    code: element.weather.elementAt(0).icon,
+                    height: 50,
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:weather_app/domain/day_weather.dart';
 import 'package:weather_app/domain/full_day_weather_details.dart';
+import 'package:weather_app/domain/weather_api_response.dart';
 import 'package:weather_app/infrastructure/weather_repository.dart';
-import 'package:weather_app/presentation/_components/weather_icon.dart';
+import 'package:weather_app/presentation/home/components/today_details.dart';
 import 'package:weather_app/presentation/home/components/weather_days_list.dart';
 
 class Home extends StatefulWidget {
@@ -16,18 +16,30 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   FullDayWeatherDetails? today;
   List<FullDayWeatherDetails>? nextDays;
+  ResponseCity? city;
+
+  String search = '';
 
   @override
   void initState() {
     super.initState();
 
-    loadData();
+    // loadData();
   }
 
-  Future<void> loadData() async {
-    final response = await WeatherRepository().getWeatherByCity(name: 'London');
+  // TODO: spostare questo loadData da un'altra parte e riportare a stateless widget
+  Future<void> loadData(String searchValue) async {
+    final response =
+        await WeatherRepository().getWeatherByCity(name: searchValue);
 
-    final dayWeatherDetailsList = response.list
+    updateFromWeatherData(response.list);
+    city = response.city;
+
+    setState(() {});
+  }
+
+  void updateFromWeatherData(List<DayWeatherData> weatherDataList) {
+    final dayWeatherDetailsList = weatherDataList
         .fold(
           <DateTime, List<DayWeatherData>>{},
           (Map<DateTime, List<DayWeatherData>> map, element) {
@@ -52,13 +64,9 @@ class _HomeState extends State<Home> {
             ))
         .toList();
 
-    // TODO: get today
     today = dayWeatherDetailsList.first;
 
-    // TODO: get next 5 days
     nextDays = dayWeatherDetailsList.skip(1).toList();
-
-    setState(() {});
   }
 
   @override
@@ -70,7 +78,17 @@ class _HomeState extends State<Home> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            if (today != null) Today(today!),
+            TextField(
+              decoration: const InputDecoration(prefixIcon: Icon(Icons.search)),
+              onSubmitted: (value) {
+                loadData(value);
+              },
+            ),
+            if (today != null)
+              TodayDetails(
+                day: today!,
+                city: city!,
+              ),
             if (nextDays == null) const Text('Loading...'),
             if (nextDays != null) WeatherDaysList(days: nextDays!),
             // Let the colum scroll a bit longer when there's a system UI element at the bottom (e.g. on iPhoneX and later)
@@ -78,56 +96,6 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class Today extends StatelessWidget {
-  final FullDayWeatherDetails day;
-
-  const Today(
-    this.day, {
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final now = DateTime.now();
-
-    return Column(
-      children: [
-        const Text('London'),
-        Text(DateFormat('EEE, d MMMM HH:mm').format(now)),
-        Row(
-          children: [
-            if (day.icon != null) WeatherIcon(code: day.icon!),
-            Text('${day.dayDetails.elementAt(0).main.temp.round()}°'),
-            Text(day.dayDetails.elementAt(0).weather.elementAt(0).description),
-          ],
-        ),
-        SizedBox(
-          height: 100,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: day.dayDetails.length,
-            itemBuilder: (context, index) {
-              final element = day.dayDetails.elementAt(index);
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(DateFormat('HH:mm').format(
-                      DateTime.fromMillisecondsSinceEpoch(element.dt * 1000))),
-                  Text('${element.main.temp.round()}°'),
-                  WeatherIcon(
-                    code: element.weather.elementAt(0).icon,
-                    height: 50,
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ],
     );
   }
 }
