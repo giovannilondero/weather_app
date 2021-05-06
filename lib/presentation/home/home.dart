b59ae7bfe23d36c7934683a5e783955c04421cf7
Giovanni Londero <londero.giovanni@gmail.com>
2021-05-06 21:11:02 +0200

    fix(home): fix scrolling issues when keyboard is open and fix selectedDayIndex issue when searching for a second city

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,5 +1,7 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/application/weather/weather_cubit.dart';
 
 // Project imports:
 import 'package:weather_app/presentation/home/components/search_bar.dart';
@@ -21,7 +23,6 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-
     weatherDaysPageViewController.addListener(updateSelectedDayIndex);
   }
 
@@ -40,6 +41,12 @@ class _HomeState extends State<Home> {
     }
   }
 
+  void resetSelectedDayIndex() {
+    setState(() {
+      selectedDayIndex = 0;
+    });
+  }
+
   void navigateToDay(int index) {
     weatherDaysPageViewController.animateToPage(
       index,
@@ -50,31 +57,51 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        bottom: false, // set at the bottom of the scroll view
-        child: DefaultTextStyle(
-          // TODO: move color somewhere else
-          style: const TextStyle(color: Color(0xff3d4b50)),
-          child: Column(
-            children: [
-              // TODO: aggiungere pulsante per trovare posizione all'inizio, ma solo quando non ho ancora acconsentito
-              // TODO: prendere subito la posizione corrente se posso
-              const SearchBar(),
-              Expanded(
-                flex: 3,
-                child: WeatherDaysPageView(
-                  controller: weatherDaysPageViewController,
+    final mediaQuery = MediaQuery.of(context);
+    const searchInputHeight = 50; // TODO: move this const
+
+    return BlocListener<WeatherCubit, WeatherState>(
+      listener: (context, state) {
+        // When the state changes in anyway, reset the selectedDayIndex to avoid
+        // having a selectedDay in the bottom list different than the details in PageView.
+        resetSelectedDayIndex();
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size(double.infinity, 200),
+          child: Padding(
+            padding: EdgeInsets.only(top: mediaQuery.padding.top),
+            child: const SearchBar(),
+          ),
+        ),
+        body: SafeArea(
+          bottom: false, // set at the bottom of the scroll view
+          child: DefaultTextStyle(
+            style: const TextStyle(color: Color(0xff3d4b50)),
+            child: SingleChildScrollView(
+              child: SizedBox(
+                height: mediaQuery.size.height -
+                    mediaQuery.padding.top -
+                    searchInputHeight,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: WeatherDaysPageView(
+                        controller: weatherDaysPageViewController,
+                      ),
+                    ),
+                    Expanded(
+                      child: WeatherDaysList(
+                        selectedIndex: selectedDayIndex,
+                        controller: weatherDaysListController,
+                        onTap: navigateToDay,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Expanded(
-                child: WeatherDaysList(
-                  selectedIndex: selectedDayIndex,
-                  controller: weatherDaysListController,
-                  onTap: navigateToDay,
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
